965012df693c5ed5713cee220ac0ca42dddd4167
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-09-10 20:40:42 +0530

    initial commit

---
+++ models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create and export the Blog model
+module.exports = mongoose.model("Blog", blogSchema);
