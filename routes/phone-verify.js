58c4ead9f1fa4c6839b868c10088e33e8a585ff9
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-09-10 18:45:05 +0530

    initial commit

--- routes/phone-verify.js
+++ routes/phone-verify.js
@@ -10,32 +10,32 @@ const client = new MongoClient(uri, {
 });
 
 router.post("/", async (req, res) => {
-    const { phoneVerificationCode } = req.body;
-  
-    try {
-      await client.connect();
-      const db = client.db("portfolio-project");
-      const usersCollection = db.collection("users");
-  
-      // Find the user with the matching phone verification code
-      const user = await usersCollection.findOne({ phoneVerificationCode });
-  
-      if (!user) {
-        res.status(404).json({ message: "Invalid verification code" });
-        return;
-      }
-  
-      // Update the user's "isPhoneVerified" field to true
-      await usersCollection.updateOne(
-        { _id: user._id },
-        { $set: { isPhoneVerified: true } }
-      );
-  
-      res.status(200).json({ message: "Phone number verified successfully" });
-    } catch (error) {
-      console.error("Error verifying phone number:", error);
-      res.status(500).json({ message: "Internal server error" });
+  const { phoneVerificationCode } = req.body;
+
+  try {
+    await client.connect();
+    const db = client.db("portfolio-project");
+    const usersCollection = db.collection("users");
+
+    // Find the user with the matching phone verification code
+    const user = await usersCollection.findOne({ phoneVerificationCode });
+
+    if (!user) {
+      res.status(404).json({ message: "Invalid verification code" });
+      return;
     }
-  });
-  
+
+    // Update the user's "isPhoneVerified" field to true
+    await usersCollection.updateOne(
+      { _id: user._id },
+      { $set: { isPhoneVerified: true } }
+    );
+
+    res.status(200).json({ message: "Phone number verified successfully" });
+  } catch (error) {
+    console.error("Error verifying phone number:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
