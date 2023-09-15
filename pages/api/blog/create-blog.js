adf6bed7bb5c301ccd777281105cbb1c52028129
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-09-16 00:16:34 +0530

    initial commit

--- pages/api/blog/create-blog.js
+++ pages/api/blog/create-blog.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const router = express.Router();
 const uri = process.env.MONGODB_URI;
@@ -11,10 +12,26 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
+// Configure multer to store uploaded files in a specific directory
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Create an 'uploads' directory in your project
+  },
+  filename: function (req, file, cb) {
+    const extension = file.originalname.split(".").pop();
+    const filename = `${uuidv4()}.${extension}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 router.use(bodyParser.json());
 
-router.post("/", async (req, res) => {
+// Use the 'upload' middleware to handle file uploads
+router.post("/", upload.single("coverImage"), async (req, res) => {
   const { title, content, email } = req.body; // userEmail is used to identify the user
+  const coverImage = req.file ? req.file.filename : null; // Get the uploaded file's filename
 
   try {
     await client.connect();
@@ -33,13 +50,14 @@ router.post("/", async (req, res) => {
     // Generate a unique blog ID
     const blogId = uuidv4();
 
-    // Insert the blog document with the generated blog ID and user's name as author
+    // Insert the blog document with the generated blog ID, user's name as author, and cover image filename
     const result = await blogsCollection.insertOne({
       blogId: blogId,
       title: title,
       content: content,
       author: `${user.firstName} ${user.lastName}`,
       email: `${user.email}`,
+      coverImage: coverImage, // Store the cover image filename
       createdAt: new Date(),
     });
 
