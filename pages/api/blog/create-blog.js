e9fdee194c2454ca579e0d3bc7ff077b3af8e632
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-09-16 14:20:58 +0530

    initial commit

--- pages/api/blog/create-blog.js
+++ pages/api/blog/create-blog.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb"); // Import ObjectId from mongodb
 const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
 const multer = require("multer");
+const jwt = require("jsonwebtoken"); // Import jwt module
 
 const router = express.Router();
 const uri = process.env.MONGODB_URI;
@@ -30,8 +31,9 @@ router.use(bodyParser.json());
 
 // Use the 'upload' middleware to handle file uploads
 router.post("/", upload.single("coverImage"), async (req, res) => {
-  const { title, content, email } = req.body; // userEmail is used to identify the user
-  const coverImage = req.file ? req.file.filename : null; // Get the uploaded file's filename
+  const token = req.header("x-auth-token");
+  const { title, content } = req.body;
+  const coverImage = req.file ? req.file.filename : null;
 
   try {
     await client.connect();
@@ -39,37 +41,44 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
     const blogsCollection = db.collection("blogs");
     const usersCollection = db.collection("users");
 
-    // Find the user based on their email
-    const user = await usersCollection.findOne({ email: email });
+    // Verify the JWT token
+    jwt.verify(token, "XXR", async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
 
-    if (!user) {
-      res.status(404).json({ message: "User not found" });
-      return;
-    }
+      const userId = decoded.userId; // userId is a string
+      const objectIdUserId = new ObjectId(userId);
 
-    // Generate a unique blog ID
-    const blogId = uuidv4();
+      const user = await usersCollection.findOne({ _id: objectIdUserId });
 
-    // Insert the blog document with the generated blog ID, user's name as author, and cover image filename
-    const result = await blogsCollection.insertOne({
-      blogId: blogId,
-      title: title,
-      content: content,
-      author: `${user.firstName} ${user.lastName}`,
-      email: `${user.email}`,
-      coverImage: coverImage, // Store the cover image filename
-      createdAt: new Date(),
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+
+      // Generate a unique blog ID
+      const blogId = uuidv4();
+
+      // Insert the blog document with the generated blog ID, user's name as author, and cover image filename
+      const result = await blogsCollection.insertOne({
+        blogId: blogId,
+        title: title,
+        content: content,
+        author: `${user.firstName} ${user.lastName}`,
+        email: `${user.email}`,
+        coverImage: coverImage, // Store the cover image filename
+        createdAt: new Date(),
+      });
+
+      console.log("Blog inserted:", result.insertedId);
+      res.status(200).json({
+        message: "Blog posted successfully",
+        blogId: blogId,
+      });
     });
-
-    console.log("Blog inserted:", result.insertedId);
-    res.status(200).json({
-      message: "Blog posted successfully",
-      blogId: blogId,
-    });
-
-    client.close();
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+  }  catch (error) {
+    console.error("Error connecting to MongoDB Atlas:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
